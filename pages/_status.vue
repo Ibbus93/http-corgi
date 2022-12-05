fdce7be5ee157cc66e833926be655512bcaa495a
Federico Ibba <federico.ibba@spindox.it>
2022-12-05 22:59:40 +0100

    chore: add list of accepted statuses, add first two code images

---
+++ pages/_status.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="status-container">
+    <img :src="imgPath" />
+  </div>
+</template>
+
+<script lang="ts">
+import { acceptedStatuses } from '@/utils/constants'
+
+export default {
+  computed: {
+    status() {
+      const statusParam = Number(this.$route.params?.status) ?? 404
+
+      return acceptedStatuses.includes(statusParam) ? statusParam : 404
+    },
+    imgPath() {
+      return require(`../static/images/${this.status}.png`)
+    },
+  },
+}
+</script>
+
+<style>
+.status-container img {
+  max-width: 50%;
+}
+</style>
